4086df54d0d188c9dc231e22096e95276da66c4c
ptjob1 <ptjob6@gmail.com>
2019-03-01 15:16:48 +0000

    makeTripHandler function implemented and working

--- functions/index.js
+++ functions/index.js
@@ -14,72 +14,48 @@
 // Dialogflow fulfillment getting started guide:
 // https://dialogflow.com/docs/how-tos/getting-started-fulfillment
 
-'use strict';
+"use strict";
 
-const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
+const functions = require("firebase-functions");
+const { WebhookClient } = require("dialogflow-fulfillment");
+const { Card, Suggestion } = require("dialogflow-fulfillment");
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+process.env.DEBUG = "dialogflow:debug"; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
-	(request, response) => {
-		const agent = new WebhookClient({request, response});
-		console.log(
-			'Dialogflow Request headers: ' + JSON.stringify(request.headers)
-		);
-		console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  (request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log(
+      "Dialogflow Request headers: " + JSON.stringify(request.headers)
+    );
+    console.log("Dialogflow Request body: " + JSON.stringify(request.body));
 
-		function welcome(agent) {
-			agent.add(`Welcome to my agent!`);
-		}
+    //default handlers
+    function welcome(agent) {
+      agent.add(`Welcome to my agent!`);
+    }
 
-		function fallback(agent) {
-			agent.add(`I didn't understand`);
-			agent.add(`I'm sorry, can you try again?`);
-		}
+    function fallback(agent) {
+      agent.add(`I didn't understand`);
+      agent.add(`I'm sorry, can you try again?`);
+    }
 
-		// // Uncomment and edit to make your own intent handler
-		// // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-		// // below to get this function to be run when a Dialogflow intent is matched
-		function yourFunctionHandler(agent) {
-			agent.add(
-				`This message is from Dialogflow's Cloud Functions for Firebase inline editor!`
-			);
-			agent.add(
-				new Card({
-					title: `Title: this is a card title`,
-					imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
-					text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-					buttonText: 'This is a button',
-					buttonUrl: 'https://docs.dialogflow.com/',
-				})
-			);
+    //our intent handler - lets see if it works
+    function makeTripHandler(agent) {
+      agent.add(
+        `This message is from Pete and Seb from the other side of the net!`
+      );
+    }
 
-			// agent.add(new Suggestion(`Quick Reply`));
-			// agent.add(new Suggestion(`Suggestion`));
-			// agent.setContext({
-			// 	name: 'weather',
-			// 	lifespan: 2,
-			// 	parameters: {city: 'Rome'},
-			// });
-		}
-
-		// // Uncomment and edit to make your own Google Assistant intent handler
-		// // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-		// // below to get this function to be run when a Dialogflow intent is matched
-		function googleAssistantHandler(agent) {
-			let conv = agent.conv(); // Get Actions on Google library conv instance
-			conv.ask('Hello from the Actions on Google client library!'); // Use Actions on Google library
-			agent.add(conv); // Add Actions on Google library responses to your agent's response
-		}
-
-		// Run the proper function handler based on the matched Dialogflow intent name
-		let intentMap = new Map();
-		intentMap.set('Default Welcome Intent', welcome);
-		intentMap.set('Default Fallback Intent', fallback);
-		// intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-		// intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
-		agent.handleRequest(intentMap);
-	}
+    // Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map();
+    console.log("intentMap created");
+    intentMap.set("Default Welcome Intent", welcome);
+    console.log("Default Welcome Intent");
+    intentMap.set("Default Fallback Intent", fallback);
+    console.log("Default Fallback Intent");
+    intentMap.set("Make Trip", makeTripHandler);
+    console.log("Pete and Seb's agent >>>>>> ", agent);
+    agent.handleRequest(intentMap);
+  }
 );
